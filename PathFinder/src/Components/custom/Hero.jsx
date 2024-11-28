e3d60c92457d9abf097c9707283fb0ab17d163f8
Namrata Roy <royrio2000@gmail.com>
2024-11-28 23:13:58 +0530

    button

--- PathFinder/src/Components/custom/Hero.jsx
+++ PathFinder/src/Components/custom/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 function Hero() {
   return (
@@ -8,8 +9,9 @@ function Hero() {
       className='font-extrabold text-[45px] text-center mt-16'>
         <span className='text-[#2531be]'>Discover Your Next Adventure With AI : </span>Personalized Itinearies at Your Fingertips</h1>
       <p className='text-xl text-gray-500 text-center'>Your Personal Trip Planner and Travel curator, creating custom itineraies tailored to your interests and budget</p>
-
+      <Link to={'/createTrip'}>
       <Button>Get Started, It's Free</Button>
+      </Link>
     </div>
   )
 }
