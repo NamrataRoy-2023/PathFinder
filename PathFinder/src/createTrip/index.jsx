5aad4f8208cc674fae900ea3e458538382e18e1d
Namrata Roy <royrio2000@gmail.com>
2024-12-15 00:44:28 +0530

    map api setting

--- PathFinder/src/createTrip/index.jsx
+++ PathFinder/src/createTrip/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
+import { useState } from 'react';
+
+
 function CreateTrip() {
+  const [place,setPlace]=useState();
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>
@@ -15,7 +19,12 @@ function CreateTrip() {
           <h2 className='text-xl my-3 font-medium'>
             What is your Choice of Destination?
           </h2>
-          <GooglePlacesAutocomplete/>
+          <GooglePlacesAutocomplete
+          apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+          selectProps={{
+            place,
+            onChange:(v)=>{setPlace(v);console.log(v)}
+          }}/>
         </div>
       </div>
     
