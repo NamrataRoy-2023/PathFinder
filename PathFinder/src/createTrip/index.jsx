c58b50bbabf22928603d2fb7035b8c116cc127e0
Namrata Roy <royrio2000@gmail.com>
2024-12-15 21:17:36 +0530

    initial trip planner UI Page

--- PathFinder/src/createTrip/index.jsx
+++ PathFinder/src/createTrip/index.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 import { Input } from '@/Components/ui/input';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { SelectBudgetOptions } from '@/constants/options';
 import { SelectTravelsList } from '@/constants/options';
 import { Button } from '@/Components/ui/button';
 
 function CreateTrip() {
   const [place,setPlace]=useState();
+
+  const [formData,setFormData] = useState([]);
+
+  const handleInputChange = (name,value)=>{
+    setFormData({
+      ...formData,
+      [name]:value
+    })
+  }
+
+  useEffect(()=>{
+    console.log(formData);
+  },[formData])
+
+  const onGenerateTrip = ()=>{
+    if(formData?.noOfDays>10){
+      return;
+    }
+    console.log(formData);
+  }
+
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
-      <h2 className='font-bold text-3xl'>
+      <h2 className='font-bold text-3xl text'>
         Tell Us Your Travel Preferences 🚀
       </h2>
       <p className='mt-3 text-gray-500 text-xl'>
@@ -18,7 +39,7 @@ function CreateTrip() {
       </p>
 
 
-      <div className='mt-20 flex flex-col gap-10'>
+      <div className='mt-20 flex flex-col gap-10 text'>
 {/* SELECT PLACE */}
         <div>
           <h2 className='text-xl my-3 font-medium'>
@@ -28,7 +49,7 @@ function CreateTrip() {
           apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
           selectProps={{
             place,
-            onChange:(v)=>{setPlace(v);console.log(v)}
+            onChange:(v)=>{setPlace(v); handleInputChange('location',v)}
           }}/>
         </div>
 {/* SELECT NO. OF DAYS */}
@@ -36,7 +57,9 @@ function CreateTrip() {
           <h2 className='text-xl my-3 font-medium'>
             How many days are you planning your trip?
           </h2>
-          <Input placeholder={'Ex : 3'} type="number"/>
+          <Input placeholder={'Ex : 3'} type="number"
+            onChange={(e)=>handleInputChange('noOfDays',e.target.value)}
+          />
         </div>
       
 
@@ -47,7 +70,12 @@ function CreateTrip() {
         </h2>
         <div className='grid grid-cols-3 gap-5 mt-5'>
           {SelectBudgetOptions.map((item,index)=>(
-            <div key={index} className='p-4 border rounded-lg hover:shadow-lg cursor-pointer'>
+            <div key={index} 
+              onClick={()=>handleInputChange('budget',item.title)}
+            className={`p-4 border rounded-lg 
+              hover:shadow-lg cursor-pointer
+              ${formData?.budget==item.title&&'shadow-lg border-black'}
+              `}>
               <h2 className='text-4xl'>{item.icon}</h2>
               <h2 className='font-bold text-lg'>{item.title}</h2>
               <h2 className='text-sm text-gray-500'>{item.desc}</h2>
@@ -63,7 +91,12 @@ function CreateTrip() {
         </h2>
         <div className='grid grid-cols-3 gap-5 mt-5'>
           {SelectTravelsList.map((item,index)=>(
-            <div key={index} className='p-4 border rounded-lg hover:shadow-lg cursor-pointer'>
+            <div key={index}
+            onClick={()=>handleInputChange('traveller',item.people)}
+            className={`p-4 border rounded-lg 
+              hover:shadow-lg cursor-pointer
+              ${formData?.traveller==item.people&&'shadow-lg border-black'}
+            `}>
               <h2 className='text-4xl'>{item.icon}</h2>
               <h2 className='font-bold text-lg'>{item.title}</h2>
               <h2 className='text-sm text-gray-500'>{item.desc}</h2>
@@ -76,7 +109,7 @@ function CreateTrip() {
 
 {/* BUTTON */}
       <div className='my-10 justify-end flex'>
-      <Button>Generate Trip</Button>
+      <Button onClick={onGenerateTrip}>Generate Trip</Button>
       </div>
 
     </div>
