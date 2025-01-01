53aaaf100e14dc1786c87cd4ce4982f6f1fa914d
Namrata Roy <royrio2000@gmail.com>
2025-01-01 12:45:58 +0530

    Footer

--- PathFinder/src/view-trip/[tripId]/index.jsx
+++ PathFinder/src/view-trip/[tripId]/index.jsx
@@ -6,6 +6,7 @@ import { toast } from 'sonner';
 import InfoSection from '../components/InfoSection';
 import Hotels from '../components/Hotels';
 import PlacesToVisit from '../components/PlacesToVisit';
+import Footer from '../components/Footer';
 
 function Viewtrip() {
     const {tripId}=useParams();
@@ -41,6 +42,7 @@ function Viewtrip() {
         <PlacesToVisit trip={trip}/>
 
       {/* FOOTER */}
+        <Footer trip={trip}/>
 
     </div>
   )
